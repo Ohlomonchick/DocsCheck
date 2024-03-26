3fbbffad1488610ebb95e2fa99d9c07892626480
Ohlomonchick <proskurin.dima16@gmail.com>
2024-03-26 19:50:04 +0300

    Добавлен класс проверки ТЗ. Публикация на pypi.

--- test.py
+++ test.py
@@ -1,6 +1,9 @@
 from docsCheck.runners import run_check
 
 
-verdict = run_check("src/docsCheck/samples/Система_для_автоматического_перелистывания_нот_на_планшете_актуальное.docx")
+verdict = run_check(
+    "samples/Система_для_автоматического_перелистывания_нот_на_планшете_актуальное.docx",
+    doc_type="ТЗ"
+)
 for message in verdict.messages:
     print(message)
